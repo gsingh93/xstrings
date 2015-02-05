6ddf3719e16c6aa8f9c0bd40d7edef5e9f8a7d82
Gulshan Singh <gulshan@umich.edu>
2015-02-04 19:46:02 -0500

    Updated README, changed output words to output all strings

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Extended Strings [![Build Status](https://travis-ci.org/gsingh93/xstrings.svg?branch=master)](https://travis-ci.org/gsingh93/xstrings)
 ================
 
-This program searches the given file for binary strings, hex strings, base64 strings, and English words. It was created for use in [CTF](https://ctftime.org/ctf-wtf/) competitions, but may have other uses.
+This program searches the given file for binary strings, hex strings, and base64 strings. It was created for use in [CTF](https://ctftime.org/ctf-wtf/) competitions, but may have other uses.
 
-To build, run `make` and ignore all warnings. Run `./xstrings -h` to see a list of command line options. The code has been tested with 0.11.0-pre-nightly on Linux.
+To build, run `cargo build`. `./target/xstrings -h` to see a list of command line options.
 
 TODO
 ----
@@ -11,3 +11,4 @@ TODO
 2. Implement searching for words. Allow the user to optionally specify a path to a dictionary as an argument to -w. Package default dictionary with program.
 3. Add an option for not showing duplicate matches. A string could be a valid binary string as well as a valid base64 string. We should add an option to only show unique strings (or make this default and add a --verbose flag to show all duplicates).
 4. Add support for searching through multiple files.
+5. Implement filters (i.e. output all strings containing only letters).
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(core, io, os, path, rustc_private)]
+#![feature(core, collections, io, os, rustc_private)]
 
 extern crate getopts;
 extern crate regex;
@@ -6,10 +6,12 @@ extern crate regex;
 use regex::Regex;
 
 use std::os;
-use std::old_io::File;
+use std::old_io::process::Command;
 use std::string::String;
 
-use getopts::{getopts, optflag, reqopt, usage, Matches};
+use std::collections::btree_set::BTreeSet;
+
+use getopts::{getopts, optflag, usage, Matches};
 
 fn main() {
     parse_args();
@@ -19,39 +21,38 @@ fn main() {
         None => return
     };
     let filename = &*matches.free[1];
-    let text = read_text(filename);
+    let strings = get_strings(filename);
+
     if matches.opts_present(&["b".to_string(), "x".to_string(), "s".to_string(),
                               "w".to_string()]) {
         if matches.opt_present("b") {
-            find_binary(&*text);
+            find_binary(&strings);
         }
         if matches.opt_present("x") {
-            find_hex(&*text);
+            find_hex(&strings);
         }
         if matches.opt_present("s") {
-            find_base64(&*text);
+            find_base64(&strings);
         }
         if matches.opt_present("w") {
-            find_words(&*text);
+            println!("All strings:");
+            print_set(&strings);
         }
     } else {
-        // find_words(&text);
-        find_hex(&*text);
-        find_base64(&*text);
-        find_binary(&*text);
+        find_hex(&strings);
+        find_base64(&strings);
+        find_binary(&strings);
     }
 }
 
 fn parse_args() -> Option<Matches> {
     let args = os::args();
 
-    let opts = [optflag("b", "binary", "Output binary matches"),
-                optflag("x", "hex",    "Output word matches"),
-                optflag("s", "base64", "Output base64 matches"),
-                reqopt("w", "words",  "Output English word matches. \
-                                       Dictionary file should contain one word per line",
-                       "DICTIONARY"),
-                optflag("h", "help",   "Print this help message")];
+    let opts = [optflag("b", "binary",  "Output binary matches"),
+                optflag("x", "hex",     "Output word matches"),
+                optflag("s", "base64",  "Output base64 matches"),
+                optflag("w", "strings", "Output strings using the `strings` command"),
+                optflag("h", "help",    "Print this help message")];
 
     let matches = match getopts(args.as_slice(), &opts) {
         Ok(matches) => matches,
@@ -65,60 +66,66 @@ fn parse_args() -> Option<Matches> {
     }
 
     if matches.free.len() != 2 {
-        fail("program only checks one file, multiple arguments provided");
+        panic!("exactly one file must be provided");
     }
 
     Some(matches)
 }
 
-fn read_text(filename: &str) -> String {
-    let path = Path::new(filename);
-    let mut file = match File::open(&path) {
-        Ok(file) => file,
-        Err(e) => fail(e.desc)
-    };
-
-    match file.read_to_string() {
-        Ok(text) => text,
-        Err(e) => fail(e.desc)
-    }
+fn print_set(set: &BTreeSet<String>) {
+   for s in set {
+       println!("{}", s);
+   }
 }
 
-fn find(text: &str, regex: &str) {
+fn find(text: &str, regex: &str, set: &mut BTreeSet<String>) {
     let re = match Regex::new(regex) {
         Ok(re) => re,
-        Err(e) => fail(&*e.msg)
+        Err(e) => panic!(e.msg)
     };
     for cap in re.captures_iter(text) {
-        println!("{:?}", cap.at(0));
+        set.insert(String::from_str(cap.at(0).unwrap()));
     }
 }
 
-fn find_binary(text: &str) {
+fn find_binary(set: &BTreeSet<String>) {
+    let mut results = BTreeSet::new();
     println!("{}", "Binary:");
-    find(text, r"[01]{3,}");
+    for string in set.iter() {
+        find(string, r"[01]{3,}", &mut results);
+    }
+    print_set(&results);
     println!("");
 }
 
-fn find_hex(text: &str) {
+fn find_hex(set: &BTreeSet<String>) {
+    let mut results = BTreeSet::new();
     println!("{}", "Hex:");
-    find(text, r"(0[xX])?[0-9a-fA-F]{2,}");
+    for string in set.iter() {
+        find(string, r"(0[xX])?[0-9a-fA-F]{2,}", &mut results);
+    }
+    print_set(&results);
     println!("");
 }
 
-fn find_base64(text: &str) {
+fn find_base64(set: &BTreeSet<String>) {
+    let mut results = BTreeSet::new();
     println!("{}", "Base64:");
-    find(text, r"(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})");
+    for string in set.iter() {
+        find(string,
+             r"(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})",
+             &mut results);
+    }
+    print_set(&results);
     println!("");
 }
 
-fn find_words(text: &str) {
-    panic!("Not implemented");
-    println!("{}", "Words:");
-    find(text, r"");
-    println!("");
-}
-
-fn fail(message: &str) -> ! {
-    panic!("Error: {}", message);
+fn get_strings(filename: &str) -> BTreeSet<String> {
+    let output = Command::new("strings").arg(filename).output();
+    let output = String::from_utf8(output.unwrap().output).unwrap();
+    let mut strings = BTreeSet::new();
+    for string in output.lines() {
+        strings.insert(String::from_str(string));
+    }
+    strings
 }
